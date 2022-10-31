e576d4cba3318d422a975f84b42cc55d7cdc0195
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-10-31 14:58:35 +0200

    Vuex concept

---
+++ src/components/ColorCode.vue
@@ -0,0 +1,26 @@
+<template>
+    <input 
+      type="text" 
+      placeholder="Enter color" 
+      v-model="$store.state.colorCode"
+    />
+</template>
+
+<script>
+export default {
+  computed: {
+    colorCode: {
+      get() {
+        return this.$store.state.colorCode
+      },
+      set(newValue) {
+        this.$store.dispatch('setColorCode', newValue)
+      }
+    }
+  } 
+}
+</script>
+
+<style>
+
+</style>
